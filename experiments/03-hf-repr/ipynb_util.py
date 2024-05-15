ade4714dff3b294c62a06633da28fa78e82e356d
David <git@dvdblk.com>
2024-05-15 05:30:59 +0200

    add almost perfect 03 shap

--- experiments/03-hf-repr/ipynb_util.py
+++ experiments/03-hf-repr/ipynb_util.py
@@ -2,12 +2,12 @@ import os
 
 # needs to be executed before importing torch or transformers
 # server specific: only use last 3 gpus (on rattle.ifi.uzh.ch)
-os.environ["CUDA_VISIBLE_DEVICES"] = "2,3,4"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "2,3,4"
 # set the home directory for huggingface transformers (where the models are saved)
 # by default this is '~/.cache/huggingface/hub'
 # see https://stackoverflow.com/questions/61798573/where-does-hugging-faces-transformers-save-models
 # server specific:
-os.environ["HF_HOME"] = "/srv/scratch2/dbielik/.cache/huggingface"
+# os.environ["HF_HOME"] = "/srv/scratch2/dbielik/.cache/huggingface"
 
 import torch
 import numpy as np
@@ -29,7 +29,7 @@ BASE_DIR_PATH = Path.cwd().parent
 # path of the data directory
 DATA_DIR_PATH = BASE_DIR_PATH / "data" / "swisstext-2024-sharedtask"
 
-CHECKPOINT_PATH = os.environ["HF_HOME"] + "/checkpoints"
+CHECKPOINT_PATH = (os.getenv("HF_HOME") or "..") + "/checkpoints"
 
 SEED = 1337
 set_seed(SEED)
